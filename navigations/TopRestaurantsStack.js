de4abcbd5c137c690ab03a488816a76841804299
Juanes <jesteban.2741@gmail.com>
2021-03-05 22:45:57 -0500

    navigator stack

---
+++ navigations/TopRestaurantsStack.js
@@ -0,0 +1,18 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import React from 'react'
+import TopRestaurants from '../screens/TopRestaurants'
+
+
+const Stack = createStackNavigator()
+
+export default function TopRestaurantsStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name = "top-restaurants"
+                component = {TopRestaurants}
+                options = {{title : "Top 5"}}
+            />
+        </Stack.Navigator>
+    )
+}
